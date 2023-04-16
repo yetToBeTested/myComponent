74ba3cbe52af6109b9bf40545cdc5f10c08d2fc0
wuwuwu <2624086659@qq.com>
2023-04-16 10:44:12 +0800

    提交button

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import myButton from './components/myButton/myButton.vue'
+
+// const searchData = ref({
+//   keyword: '',
+//   placeholder: '请输入你想要查询的关键字',
+//   options: [
+//     { label: '视频', value: 'video' },
+//     { label: '文章', value: 'article' },
+//     { label: '用户', value: 'user' }
+//   ],
+//   selectedValue: 'video'
+// })
+
+// console.log(searchData.value.keyword)
+</script>
+
+<template>
+  <!-- <searcher-bar v-model="searchData" />
+  <div>{{ searchData.selectedValue }} - {{ searchData.keyword }}</div> -->
+  <myButton type="primary">Primary</myButton>
+  <el-Button type="primary">Primary</el-Button>
+</template>
+
+<style scoped></style>
---
+++ src/components/myButton/myButton.vue
@@ -0,0 +1,98 @@
+<template>
+  <button :class="typeColor" type="button">
+    <i v-if="props.left"></i>
+    <i v-else-if="props.rigt"></i>
+    <span class><slot /></span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface IProps {
+  type?: string
+  left?: string
+  rigt?: string
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  type: 'default',
+  left: '',
+  rigt: ''
+})
+// border bg-[#409EFF]
+const colorMap = new Map()
+  .set('default', 'my-button')
+  .set('primary', 'my-button my-button--primary')
+  .set('success', 'border bg-[#67C23A]')
+  .set('info', 'border bg-[#909399]')
+  .set('warning', 'border bg-[#E6A23C]')
+  .set('danger', 'border bg-[#F56C6C]')
+
+const typeColor = computed(() => colorMap.get(props.type))
+</script>
+
+<style scoped lang="less">
+.my-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 1;
+  height: 32px;
+  white-space: nowrap;
+  cursor: pointer;
+  color: var(--el-button-text-color);
+  text-align: center;
+  box-sizing: border-box;
+  outline: 0;
+  transition: 0.1s;
+  font-weight: var(--el-button-font-weight);
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  vertical-align: middle;
+  // -webkit-appearance: none;
+  background-color: var(--el-button-bg-color);
+  border: var(--el-border);
+  border-color: var(--el-button-border-color);
+  padding: 8px 15px;
+  font-size: var(--el-font-size-base);
+  border-radius: var(--el-border-radius-base);
+}
+// .my-button {
+//   --el-button-font-weight: var(--el-font-weight-primary);
+//   --el-button-border-color: var(--el-border-color);
+//   --el-button-bg-color: var(--el-fill-color-blank);
+//   --el-button-text-color: var(--el-text-color-regular);
+//   --el-button-disabled-text-color: var(--el-disabled-text-color);
+//   --el-button-disabled-bg-color: var(--el-fill-color-blank);
+//   --el-button-disabled-border-color: var(--el-border-color-light);
+//   --el-button-divide-border-color: rgba(255, 255, 255, 0.5);
+//   --el-button-hover-text-color: var(--el-color-primary);
+//   --el-button-hover-bg-color: var(--el-color-primary-light-9);
+//   --el-button-hover-border-color: var(--el-color-primary-light-7);
+//   --el-button-active-text-color: var(--el-button-hover-text-color);
+//   --el-button-active-border-color: var(--el-color-primary);
+//   --el-button-active-bg-color: var(--el-button-hover-bg-color);
+//   --el-button-outline-color: var(--el-color-primary-light-5);
+//   --el-button-hover-link-text-color: var(--el-color-info);
+//   --el-button-active-color: var(--el-text-color-primary);
+// }
+.my-button--primary {
+  --el-button-text-color: var(--el-color-white);
+  --el-button-bg-color: var(--el-color-primary);
+  --el-button-border-color: var(--el-color-primary);
+  --el-button-outline-color: var(--el-color-primary-light-5);
+  --el-button-active-color: var(--el-color-primary-dark-2);
+  --el-button-hover-text-color: var(--el-color-white);
+  --el-button-hover-link-text-color: var(--el-color-primary-light-5);
+  --el-button-hover-bg-color: var(--el-color-primary-light-3);
+  --el-button-hover-border-color: var(--el-color-primary-light-3);
+  --el-button-active-bg-color: var(--el-color-primary-dark-2);
+  --el-button-active-border-color: var(--el-color-primary-dark-2);
+  --el-button-disabled-text-color: var(--el-color-white);
+  --el-button-disabled-bg-color: var(--el-color-primary-light-5);
+  --el-button-disabled-border-color: var(--el-color-primary-light-5);
+}
+</style>
